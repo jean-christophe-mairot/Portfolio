5676bfd3aec6b257ab92a66ccf6eb93c3070cae1
jean-christophe-mairot <60504809+jean-christophe-mairot@users.noreply.github.com>
2020-11-24 13:57:31 +0100

    Add files via upload
    first_commit

---
+++ index.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="asset/css/style.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <!-- Fork me on GitHub ribbon -->
+    <a href="https://github.com/jean-christophe-mairot" target="_blank"
+      ><img
+        style="position: fixed; top: 0; right: 0; border: 0; z-index: 99"
+        src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+        alt="Fork me on GitHub"
+        data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+    /></a>
+    <!-- Header -->
+    <header>
+      <nav>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/mairot-jc-vfx-web/"
+            target="_blank"
+            >LinkedIn</a
+          >
+        </li>
+      </nav>
+    </header>
+
+    <!-- Slider -->
+    <div id="slider">
+    <!-- content-txt -->
+      <div class="slides">
+        <div class="slider">
+          <div class="legend"></div>
+          <div class="content">
+            <div class="content-txt">
+              <h1>JEAN CHRISTOPHE MAIROT</h1>
+              <h2>
+                Innovant et proactif, capable de m'adapter et de me former
+                rapidement. Ma formation est essentiellement personnelle.
+                Aujourd'hui, je souhaite m'engager dans un nouveau challenge en
+                tant que développeur web Front end & Back end.
+              </h2>
+            </div>
+          </div>
+          <div class="image">
+            <img src="img/bandocode_carousel.jpg" />
+          </div>
+        </div>
+        <div class="slider">
+          <div class="legend"></div>
+          <div class="content">
+            <div class="content-txt">
+              <h1>GRAPHISTE VFX & RESTAURATION NUMERIQUE</h1>
+              <h2>
+                Créatif, inventif, imaginatif et avoir de réelles compétences
+                artistiques pour être sensible à l'image et aux transformations
+                qu'elle peut nécessiter.
+              </h2>
+            </div>
+          </div>
+          <div class="image">
+            <img src="img/bandocinema_carousel.jpg" />
+          </div>
+        </div>
+        <div class="slider">
+          <div class="legend"></div>
+          <div class="content">
+            <div class="content-txt">
+              <h1>DEVELOPPEUR WEB</h1>
+              <h2>
+                Mes qualités pour ce nouveau virage professionnel sont : <br />
+                Le sens de la logique, la prise de recul, <br />
+                L’écoute & l’empathie <br />
+                La soif d’apprendre & L’autonomie
+              </h2>
+            </div>
+          </div>
+          <div class="image">
+            <img src="img/bandogamedesign_carousel.jpg" />
+          </div>
+        </div>
+        <div class="slider">
+          <div class="legend"></div>
+          <div class="content">
+            S
+            <div class="content-txt">
+              <h1>GAME DESIGNER</h1>
+              <h2>
+                Je devais avoir un bon esprit de synthèse et être un bon
+                communiquant afin de transmettre de manière claire et concise
+                mes idées au reste de l'équipe.
+              </h2>
+            </div>
+          </div>
+          <div class="image">
+            <img src="img/bandocv_carousel.jpg" />
+          </div>
+        </div>
+      </div>
+      <div class="switch">
+        <ul>
+          <li>
+            <div class="on"></div>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+    <!-- cards -->
+    <h2 class="titleJob">DEVELOPPEUR WEB</h2>
+
+    <ul class="card-list">
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+          style="background-image: url(/img/gamedesign_carousel.jpg)"
+          data-image-full="/img/gamedesign_carousel.jpg"
+        >
+          <img src="/img/gamedesign_carousel.jpg" alt="Psychopomp" />
+        </a>
+        <a
+          class="card-description"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+        >
+          <h2>Psychopomp</h2>
+          <p>Japanese Breakfast</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg"
+            alt="let's go"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+        >
+          <h2>let's go</h2>
+          <p>In Love With A Ghost</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg"
+            alt="The Beautiful Game"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+        >
+          <h2>The Beautiful Game</h2>
+          <p>Vulfpeck</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg"
+            alt="Jane Doe"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+        >
+          <h2>Jane Doe</h2>
+          <p>Converge</p>
+        </a>
+      </li>
+    </ul>
+    <!-- cinema fx -->
+    <h2 class="titleJob">GRAPHISTE VFX & RESTORATION NUMERIQUE</h2>
+    <ul class="card-list">
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+          style="background-image: url(/img/gamedesign_carousel.jpg)"
+          data-image-full="/img/gamedesign_carousel.jpg"
+        >
+          <img src="/img/gamedesign_carousel.jpg" alt="Psychopomp" />
+        </a>
+        <a
+          class="card-description"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+        >
+          <h2>Psychopomp</h2>
+          <p>Japanese Breakfast</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg"
+            alt="let's go"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+        >
+          <h2>let's go</h2>
+          <p>In Love With A Ghost</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg"
+            alt="The Beautiful Game"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+        >
+          <h2 class="titleJob">The Beautiful Game</h2>
+          <p>Vulfpeck</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg"
+            alt="Jane Doe"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+        >
+          <h2>Jane Doe</h2>
+          <p>Converge</p>
+        </a>
+      </li>
+    </ul>
+    <!-- GAME DESIGNER -->
+    <h2 class="titleJob">GAME DESIGNER</h2>
+    <ul class="card-list">
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+          style="background-image: url(/img/gamedesign_carousel.jpg)"
+          data-image-full="/img/gamedesign_carousel.jpg"
+        >
+          <img src="/img/gamedesign_carousel.jpg" alt="Psychopomp" />
+        </a>
+        <a
+          class="card-description"
+          href="https://michellezauner.bandcamp.com/album/psychopomp-2"
+          target="_blank"
+        >
+          <h2>Psychopomp</h2>
+          <p>Japanese Breakfast</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-100.jpg"
+            alt="let's go"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://inlovewithaghost.bandcamp.com/album/lets-go"
+          target="_blank"
+        >
+          <h2>let's go</h2>
+          <p>In Love With A Ghost</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-100.jpg"
+            alt="The Beautiful Game"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://vulfpeck.bandcamp.com/album/the-beautiful-game"
+          target="_blank"
+        >
+          <h2>The Beautiful Game</h2>
+          <p>Vulfpeck</p>
+        </a>
+      </li>
+
+      <li class="card">
+        <a
+          class="card-image"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+          style="
+            background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg);
+          "
+          data-image-full="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-500.jpg"
+        >
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-100.jpg"
+            alt="Jane Doe"
+          />
+        </a>
+        <a
+          class="card-description"
+          href="https://convergecult.bandcamp.com/album/jane-doe"
+          target="_blank"
+        >
+          <h2>Jane Doe</h2>
+          <p>Converge</p>
+        </a>
+      </li>
+    </ul>
+
+    <!-- integration de code pen -->
+    <!-- faire un compte code pen puis integrer le code sur notre page  -->
+    <!--  -->
+    <footer></footer>
+    <!-- script -->
+    <script src="asset/js/cards.js"></script>
+  </body>
+</html>
